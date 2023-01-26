18b439a273f11306576b8b9a8a24b089267c3dde
disko998 <disko998@gmail.com>
2023-01-26 16:08:33 +0100

    setup tailwind

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+
+const withVideos = require('next-videos')
+
+const nextConfig = withVideos({
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        })
+
+        return config
+    },
+})
 
 module.exports = nextConfig
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <div>
+      <h1 className="text-9xl font-bold underline">Hello world!</h1>
+    </div>
+  )
+}

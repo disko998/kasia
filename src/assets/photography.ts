a569bc46654ca77ddaf1a23d788276efa68e5706
Tizu18 <alen_babanovic123@hotmail.com>
2023-03-09 15:11:32 +0100

    categories animation

--- src/assets/photography.ts
+++ src/assets/photography.ts
@@ -4,16 +4,41 @@ import image3 from '../../public/images/photography/3.jpg'
 import image4 from '../../public/images/photography/4.jpg'
 import image5 from '../../public/images/photography/5.jpg'
 import image6 from '../../public/images/photography/6.jpg'
+import image7 from '../../public/images/photography/2.jpg'
+import image8 from '../../public/images/photography/4.jpg'
+import image9 from '../../public/images/photography/6.jpg'
+import image10 from '../../public/images/photography/5.jpg'
 
-enum PhotographyCategories {
+export enum PhotographyCategories {
   ALL = 'Svi albums',
   PORTRETI = 'Portreti',
   DOGADJAJI = 'Dogadjaji',
   ARHITEKTURA = 'Arhitektura'
 }
 
-const data = {
-  [PhotographyCategories.PORTRETI]: [image1, image2, image3]
+const photoData = {
+  [PhotographyCategories.ALL]: [
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10
+  ],
+  [PhotographyCategories.PORTRETI]: [image1, image2, image3],
+  [PhotographyCategories.DOGADJAJI]: [
+    image1,
+    image2,
+    image3,
+    image5,
+    image4,
+    image8
+  ],
+  [PhotographyCategories.ARHITEKTURA]: [image6, image9, image3, image2, image10]
 }
 
-export default data
+export default photoData

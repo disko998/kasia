6f7ac1f2e9703c55586caafc5078f986bfe17027
disko998 <disko998@gmail.com>
2023-03-29 01:52:30 +0200

    meta data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,20 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { imbue, jeko } from '@/styles/fonts'
+import Head from 'next/head'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`${imbue.variable} ${jeko.variable} font-sans`}>
+      <Head>
+        <title>{'Kasia - ZABELEŽI TRENUTAK SA KASIA STUDIOM'}</title>
+        <meta
+          name="description"
+          content={
+            'Naš tim je sastavljen od 3 profesionalca u svojoj oblasti: fotografija i video montaža. Svi zajedno kreiramo jedinstvene i kvalitetne usluge.'
+          }
+        />
+      </Head>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,25 @@ import { Head, Html, Main, NextScript } from 'next/document'
 const Document = () => {
   return (
     <Html>
-      <Head></Head>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
